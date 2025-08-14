3fa2bd4815358bfa5637c3298d9cd8f1c4ad9fb9
CAROLINE NARDELLI WACKERHAGE <caroline.wackerhage@alunos.sc.senac.br>
2025-08-13 21:42:59 -0300

    Criando o coração para adicionar aos favoritos

--- Receitas/receita2.js
+++ Receitas/receita2.js
@@ -17,3 +17,19 @@ function voltar() {
     alert("Abrindo painel de XP...");
   }
   
+
+  let favoritado = false;
+
+function toggleFavorito() {
+  const coracao = document.getElementById("coracao-path");
+  favoritado = !favoritado;
+
+  if (favoritado) {
+    coracao.setAttribute("fill", "#FF4E4E"); // cor vermelha preenchida
+    coracao.setAttribute("stroke", "#FF4E4E");
+  } else {
+    coracao.setAttribute("fill", "none");
+    coracao.setAttribute("stroke", "#000");
+  }
+}
+
