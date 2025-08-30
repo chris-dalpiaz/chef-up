da8fea8fb3105d88d2198b08e77116dcfb41e9ac
CHRISTOFER EZEQUIEL DALPIAZ <christofer.dalpiaz@alunos.sc.senac.br>
2025-08-29 21:00:20 -0300

    envio

--- sql/selects.sql
+++ sql/selects.sql
@@ -8,3 +8,9 @@ SELECT
 FROM ingrediente_receita ir
 LEFT JOIN ingrediente i ON ir.ingrediente_id = i.id
 LEFT JOIN ingrediente_etapa_receita ier ON ir.id = ier.ingrediente_receita_id;
+
+select a.etapa_receita_id, b.id, b.quantidade, c.nome, b.ingrediente_id from ingrediente_etapa_receita a
+inner join ingrediente_receita b on a.ingrediente_receita_id = b.id
+inner join ingrediente c on b.ingrediente_id = c.id
+where etapa_receita_id = 20;
+select * from ingrediente;
